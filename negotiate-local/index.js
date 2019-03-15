867fbda56204667187404df78d31e21a8671a44c
Anthony Chu <antchu@microsoft.com>
2019-03-14 21:35:18 -0700

    Add production endpoint

--- negotiate-local/index.js
+++ negotiate-local/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req, connectionInfo) {
-    if (!process.env.WEBSITE_AUTH_ENABLED) {
-        throw "App Service Auth disabled!";
+    if (process.env.WEBSITE_AUTH_ENABLED) {
+        throw "App Service Auth enabled!";
     }
     context.res.body = connectionInfo;
 };
